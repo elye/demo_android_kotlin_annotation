f6bbcc051a73e0be841700b2807928cc33c1fcca
Elisha Lye <elisha.lye@rea-group.com>
2022-04-13 21:02:34 +1000

    Upgrade library

--- app/src/main/java/com/elyeproj/annotationexperiment/MainActivity.kt
+++ app/src/main/java/com/elyeproj/annotationexperiment/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.elyeproj.annotationexperiment
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import com.elyeproj.annotation.CheckCamelSource
 import com.elyeproj.annotation.GenerateSource
